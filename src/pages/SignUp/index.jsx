af2fc715bf2ac966d9d4c99427bcfcb0ba4ddbd1
Michelle Cordeiro <mic321@gmail.com>
2025-06-03 12:38:33 -0300

    feat: add axios and services/api.js, implement SignUp functionality

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,7 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import { api } from '../../services/api'
 
 import { Logo } from '../../components/Logo'
 import { Button } from '../../components/Button'
@@ -7,6 +10,31 @@ import { Input } from '../../components/Input'
 import { Container, Content, Form } from './styles'
 
 export function SignUp() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSighUp() {
+    if(!name || !email || !password) {
+      return alert("Preencha todos os campos!")
+    }
+
+    api.post('/users', { name, email, password })
+      .then(() => {
+        alert('Usuário cadastrado com sucesso!')
+        navigate('/')
+      })
+      .catch(error => {
+        if(error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert('Não foi possível cadastrar')
+        }
+      })
+  }
+
   return (
     <Container>
       <Content>
@@ -14,10 +42,22 @@ export function SignUp() {
 
         <Form action=''>
           <label htmlFor='name'>Seu nome</label>
-          <Input type='name' name='name' id='name' placeholder='Exemplo: Maria da Silva' />
+          <Input
+            type='name'
+            name='name'
+            id='name'
+            placeholder='Exemplo: Maria da Silva'
+            onChange={e => setName(e.target.value)}
+          />
 
           <label htmlFor='email'>E-mail</label>
-          <Input type='email' name='email' id='email' placeholder='Exemplo: exemplo@mail.com.br' />
+          <Input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Exemplo: exemplo@mail.com.br'
+            onChange={e => setEmail(e.target.value)}
+          />
 
           <label htmlFor='password'>Senha</label>
           <Input
@@ -25,9 +65,10 @@ export function SignUp() {
             name='password'
             id='password'
             placeholder='No mínimo 6 caracteres'
+            onChange={e => setPassword(e.target.value)}
           />
 
-          <Button type='submit' title='Criar conta' />
+          <Button title='Criar conta' onClick={handleSighUp} />
         </Form>
 
         <Link to='/'>Já tenho conta</Link>
